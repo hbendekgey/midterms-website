4a4ac99652ea000bb3f66488d76bffa13ae49cc2
hbendekgey <henry.bendekgey@pomona.edu>
2018-07-26 12:15:59 -0700

    indentation, vals -> sxvals, sidebar names

--- shinyApp.R
+++ shinyApp.R
@@ -21,48 +21,54 @@ header <- dashboardHeader(
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    menuItem("Structure-X", tabName = "sx"),
+    menuItem("National Polls and District Info", tabName = "bafumi")
   )
 )
 
 body <- dashboardBody(useShinyalert(), fluidRow(
-  column(8, align="center",
-    box(width = NULL, align="left", status="primary", desc_text),
-    box(width = NULL, plotOutput("plot")),
-    box(width = NULL, 
-        h4(textOutput("interval")),
-        h4(textOutput("est")),
-        h4(textOutput("dem_win_pct"))
-    )
-  ),
-  column(4,
-    box(width = NULL, status = "warning",
-      tags$h4(tags$b("Fix up the model your way")),
-      selectInput(inputId = "pres",
-                  label = "Predict results based on Presidential...",
-                  choices = c("Approval", "Disapproval", "Net Approval", "Approval Share")),
-      checkboxInput(inputId = "structure_only", label="Use only Structural Model"),
-      actionLink("s_only_info", "What is this?"),
-      conditionalPanel(
-        condition = "!input.structure_only",
-        checkboxInput(inputId = "shift_pred", label="Shift prediction towards out-party"),
-        actionLink("shift_pred_info", "Why do this?")
+  tabItems(
+    # Structure-X
+    tabItem(tabName="sx",
+    column(8, align="center",
+      box(width = NULL, align="left", status="primary", desc_text),
+      box(width = NULL, plotOutput("plot")),
+      box(width = NULL, 
+          h4(textOutput("interval")),
+          h4(textOutput("est")),
+          h4(textOutput("dem_win_pct"))
       )
     ),
-    box(width = NULL, status = "warning",
-      sliderInput(inputId = "rdig",
-                  label = "December '17-June '18 Real Disposible Income Growth",
-                  value = 1.47, min = -1, max = 5, step=0.01),
-      sliderInput(inputId = "pres_app",
-                  label = "Trump June Approval:",
-                  value = 42, min = 30, max = 70),
-      sliderInput(inputId = "pres_dis",
-                  label = "Trump June Disapproval",
-                  value = 52, min = 30, max = 70),
-      actionButton("reset", "Reset")
+    column(4,
+      box(width = NULL, status = "warning",
+        tags$h4(tags$b("Fix up the model your way")),
+        selectInput(inputId = "pres",
+                    label = "Predict results based on Presidential...",
+                    choices = c("Approval", "Disapproval", "Net Approval", "Approval Share")),
+        checkboxInput(inputId = "structure_only", label="Use only Structural Model"),
+        actionLink("s_only_info", "What is this?"),
+        conditionalPanel(
+          condition = "!input.structure_only",
+          checkboxInput(inputId = "shift_pred", label="Shift prediction towards out-party"),
+          actionLink("shift_pred_info", "Why do this?")
+        )
+      ),
+      box(width = NULL, status = "warning",
+        sliderInput(inputId = "rdig",
+                    label = "December '17-June '18 Real Disposible Income Growth",
+                    value = 1.47, min = -1, max = 5, step=0.01),
+        sliderInput(inputId = "pres_app",
+                    label = "Trump June Approval:",
+                    value = 42, min = 30, max = 70),
+        sliderInput(inputId = "pres_dis",
+                    label = "Trump June Disapproval",
+                    value = 52, min = 30, max = 70),
+        actionButton("reset", "Reset")
+      )
     )
-  )
+  ),
+  # Bafumi, Erikson Wlezien
+  tabItem(tabName="bafumi", fluidPage()))
 ))
 
 ui <- dashboardPage(header, sidebar, body)
@@ -74,12 +80,13 @@ structure <- read_csv("~/house_forecast_2018/data/structurex.csv") %>%
 structurex <- filter(structure, year >= 2006)
 
 server <- function(input, output, session) {
-  
-  # info buttons and link
   observeEvent(input$info, {
     shinyalert("2018 House Forecasting Models", 
                info_text, type = "info")
   })
+  ###Structure-X
+  
+  # sx info buttons and link
   observeEvent(input$s_only_info, {
     shinyalert("Use Only the Structural Model", 
                s_only_text, type = "info")
@@ -106,78 +113,75 @@ server <- function(input, output, session) {
     }
   })
   
-  # regression values
-  vals <- reactiveValues()
+  # sx regression values
+  sxvals <- reactiveValues()
   observe({
-    vals$net <- input$pres_app - input$pres_dis
-    vals$share <- round(input$pres_app/ (input$pres_dis + input$pres_app) * 100)
-    vals$fit <- switch(input$pres,
+    sxvals$net <- input$pres_app - input$pres_dis
+    sxvals$share <- round(input$pres_app/ (input$pres_dis + input$pres_app) * 100)
+    sxvals$fit <- switch(input$pres,
                        "Approval" = lm(chiseats ~ midterm + rdi_growth + pres_app, data=structure),
                        "Disapproval" = lm(chiseats ~ midterm + rdi_growth + pres_dis, data=structure),
                        "Net Approval" = lm(chiseats ~ midterm + rdi_growth + pres_net, data=structure),
                        "Approval Share" = lm(chiseats ~ midterm + rdi_growth + pres_share, data=structure))
     params <- data.frame(rdi_growth=input$rdig, pres_dis = input$pres_dis, 
                          midterm = 1, pres_app=input$pres_app, 
-                         pres_net=vals$net, pres_share = vals$share)
-    interval <- predict.lm(vals$fit, params, interval = "prediction", se.fit = TRUE, level=0.9)
+                         pres_net=sxvals$net, pres_share = sxvals$share)
+    interval <- predict.lm(sxvals$fit, params, interval = "prediction", se.fit = TRUE, level=0.9)
     if(input$structure_only) {
-      vals$mean <- interval$fit[1]
-      vals$lower <- ceiling(interval$fit[2])
-      vals$upper <- floor(interval$fit[3])
-      vals$df <- interval$df
-      t.cutoff <- qt(0.95, df=vals$df)
-      vals$se <- (interval$fit[1] - interval$fit[2])/t.cutoff
+      sxvals$mean <- interval$fit[1]
+      sxvals$lower <- ceiling(interval$fit[2])
+      sxvals$upper <- floor(interval$fit[3])
+      sxvals$df <- interval$df
+      t.cutoff <- qt(0.95, df=sxvals$df)
+      sxvals$se <- (interval$fit[1] - interval$fit[2])/t.cutoff
     } else {
       roth <- c(-31,-16,-64,17,2,46)
-      fitted <- vals$fit$fitted.values[29:34]
+      fitted <- sxvals$fit$fitted.values[29:34]
       pred <- (fitted + roth)/2
       actual <- structurex$chiseats
       sxpred <- data.frame(pred, actual)
       if (input$shift_pred) {
         comb_fit <- lm(actual ~ offset(1 *pred), data=sxpred) %>% summary()
-        vals$mean <- (interval$fit[1] - 59)/2 + comb_fit$coefficients[1,1]
+        sxvals$mean <- (interval$fit[1] - 59)/2 + comb_fit$coefficients[1,1]
       } else {
         comb_fit <- lm(actual ~ 0 + offset(1 *pred), data=sxpred) %>% summary()
-        vals$mean <- (interval$fit[1] - 59)/2
+        sxvals$mean <- (interval$fit[1] - 59)/2
       }
-      vals$se <- comb_fit$sigma
-      vals$df <- comb_fit$df[2]
-      t.cutoff <- qt(0.95, df=vals$df)
-      vals$lower <- ceiling(vals$mean - t.cutoff * vals$se)
-      vals$upper <- floor(vals$mean + t.cutoff * vals$se)
+      sxvals$se <- comb_fit$sigma
+      sxvals$df <- comb_fit$df[2]
+      t.cutoff <- qt(0.95, df=sxvals$df)
+      sxvals$lower <- ceiling(sxvals$mean - t.cutoff * sxvals$se)
+      sxvals$upper <- floor(sxvals$mean + t.cutoff * sxvals$se)
     }
   })
   
+  # sx output
   output$pres <- renderText({
     switch(input$pres,
            "Approval" = paste("Trump Approval: ", input$pres_app),
            "Disapproval" = paste("Trump Disapproval: ", input$pres_dis),
-           "Net Approval" = paste("Trump Net Approval: ", vals$net),
-           "Approval Share" = paste("Trump Approval Share: ", vals$share))
+           "Net Approval" = paste("Trump Net Approval: ", sxvals$net),
+           "Approval Share" = paste("Trump Approval Share: ", sxvals$share))
   })
-  
   output$interval <- renderText({
     paste("90% confidence interval for number of Democratic seats:", 
-          194 - vals$upper, " to ", 194 - vals$lower)
+          194 - sxvals$upper, " to ", 194 - sxvals$lower)
   })
-  
   output$est <- renderText({
     paste("Estimated number of Democratic seats:", 
-          194 - round(vals$mean))
+          194 - round(sxvals$mean))
   })
-  
   output$dem_win_pct <- renderText({
     paste("Probability of Democrats taking house:", 
-          round(pt((-23.5-vals$mean)/vals$se,df=vals$df) * 100),
+          round(pt((-23.5-sxvals$mean)/sxvals$se,df=sxvals$df) * 100),
           "%")
   })
-  
   output$plot <- renderPlot({
     lower.bound <- 150
     upper.bound <- 285
     dist_df <- data.frame(dseats=c(lower.bound:upper.bound)) %>%
-      mutate(prob = 10000 * (pt((194-dseats+0.5-vals$mean)/vals$se,df=vals$df) - 
-                    pt((194-dseats-0.5-vals$mean)/vals$se,df=vals$df)))
+      mutate(prob = 10000 * (pt((194-dseats+0.5-sxvals$mean)/sxvals$se,df=sxvals$df) - 
+                    pt((194-dseats-0.5-sxvals$mean)/sxvals$se,df=sxvals$df)))
     forecast_df <- data.frame(dseats=rep(dist_df$dseats, dist_df$prob))
     ggplot(aes(x=dseats, fill=(dseats > 217)), data=forecast_df) + 
       geom_histogram(binwidth=1, aes(y=..count../sum(..count..))) + xlab("Democratic seats in house") + 
@@ -186,7 +190,6 @@ server <- function(input, output, session) {
                           breaks=c("FALSE", "TRUE"),
                           labels=c("Republican House", "Democratic House"))
   })
-  
 }
 
 shinyApp(ui = ui, server = server)
