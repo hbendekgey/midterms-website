dae395d75475e74828157877c9f4753d009423e5
hbendekgey <henry.bendekgey@pomona.edu>
2018-07-26 11:55:19 -0700

    Better arrangement of page

--- shinyApp.R
+++ shinyApp.R
@@ -6,47 +6,61 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 
-# figure out how to use updateSilderInput in order to max Presidential opinion at 100
+# add other models
+# allow for analysis of past years
+
+s_only_text <- "Half of the prediction comes from Rothenberg's rating of races as e.g. Safe-D or Tossup or Lean-R. These have historically been very accurate, but did not do well in 2016. If you do not trust pundits, check this box."
+desc_text <- "The Structure-X model predicts the house elections using Presidential approval ratings, real disposible income growth, and who holds the Presidency. If the economy is doing well, or if the President is popular, we expect the incumbent party to do better. These predictions are combined with expert opinion seat ratings (aka which seats are leaning or safe in each direction). What if you don't trust the experts? What if you think Presidential disapproval is a better predictor than Presidential approval? How would the election look different if Trump was more popular? Play around!"
+shift_pred_text <- "For the past 6 elections (as long as Rothenberg has been rating races) this model has predicted outcomes more favorable to the Presidential party every election, on average by about 6 seats. With so few data points we don't have statistical significance, (p=0.06) and it's dangerous to overfit the data, so you should only check this if you think there's a systematic reason this model might overestimate incumbent performance."
+info_text <- "For more information on these models, check out https://github.com/hbendekgey/house_forecast_2018"
 
 header <- dashboardHeader(
-  title = "Structure-X Forecasting Model",
+  title = "2018 House",
   tags$li(class = "dropdown", circleButton("info", icon("info"), size="sm", status="info"))
 )
 
-sidebar <- dashboardSidebar(disable=TRUE)
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  )
+)
 
 body <- dashboardBody(useShinyalert(), fluidRow(
   column(8, align="center",
+    box(width = NULL, align="left", status="primary", desc_text),
     box(width = NULL, plotOutput("plot")),
     box(width = NULL, 
-        h3(textOutput("interval")),
-        h3(textOutput("dem_win_pct"))
+        h4(textOutput("interval")),
+        h4(textOutput("est")),
+        h4(textOutput("dem_win_pct"))
     )
   ),
   column(4,
     box(width = NULL, status = "warning",
+      tags$h4(tags$b("Fix up the model your way")),
       selectInput(inputId = "pres",
                   label = "Predict results based on Presidential...",
                   choices = c("Approval", "Disapproval", "Net Approval", "Approval Share")),
-      sliderInput(inputId = "rdig",
-                  label = "December 2017-June 2018 Real Disposible Income Growth",
-                  value = 1.47, min = -1, max = 5, step=0.01),
-      sliderInput(inputId = "pres_app",
-                  label = "Trump June Approval:",
-                  value = 42, min = 40, max = 60),
-      sliderInput(inputId = "pres_dis",
-                  label = "Trump June Disapproval",
-                  value = 52, min = 40, max = 60)
-    ),
-    box(width= NULL, status = "warning", tags$b(textOutput("pres"))),
-    box(width = NULL, status = "warning",
       checkboxInput(inputId = "structure_only", label="Use only Structural Model"),
       actionLink("s_only_info", "What is this?"),
       conditionalPanel(
-        condition = "input.structure_only",
+        condition = "!input.structure_only",
         checkboxInput(inputId = "shift_pred", label="Shift prediction towards out-party"),
         actionLink("shift_pred_info", "Why do this?")
       )
+    ),
+    box(width = NULL, status = "warning",
+      sliderInput(inputId = "rdig",
+                  label = "December '17-June '18 Real Disposible Income Growth",
+                  value = 1.47, min = -1, max = 5, step=0.01),
+      sliderInput(inputId = "pres_app",
+                  label = "Trump June Approval:",
+                  value = 42, min = 30, max = 70),
+      sliderInput(inputId = "pres_dis",
+                  label = "Trump June Disapproval",
+                  value = 52, min = 30, max = 70),
+      actionButton("reset", "Reset")
     )
   )
 ))
@@ -59,18 +73,40 @@ structure <- read_csv("~/house_forecast_2018/data/structurex.csv") %>%
   filter(year >= 1950)
 structurex <- filter(structure, year >= 2006)
 
-s_only_text <- "The Structure-X model is formed by combining structural factors and expert opinions, namely the opinions of Rothenberg Political Report. If you do not trust pundits, check this box"
-
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  # info buttons and link
+  observeEvent(input$info, {
+    shinyalert("2018 House Forecasting Models", 
+               info_text, type = "info")
+  })
   observeEvent(input$s_only_info, {
     shinyalert("Use Only the Structural Model", 
                s_only_text, type = "info")
   })
-  
   observeEvent(input$shift_pred_info, {
-    shinyalert("Oops!", "Something went wrong.", type = "info")
+    shinyalert("Shift Prediction Towards Out-Party", 
+               shift_pred_text, type = "info")
   })
   
+  # reset sliders and make sure they don't add to > 100
+  observeEvent(input$reset, {
+    updateSliderInput(session, "rdig", value=1.47)
+    updateSliderInput(session, "pres_app", value=42)
+    updateSliderInput(session, "pres_dis", value=52)
+  })
+  observeEvent(input$pres_app, {
+    if (input$pres_app + input$pres_dis > 100) {
+      updateSliderInput(session, "pres_dis", value=100-input$pres_app)
+    }
+  })
+  observeEvent(input$pres_dis, {
+    if (input$pres_app + input$pres_dis > 100) {
+      updateSliderInput(session, "pres_app", value=100-input$pres_dis)
+    }
+  })
+  
+  # regression values
   vals <- reactiveValues()
   observe({
     vals$net <- input$pres_app - input$pres_dis
@@ -121,24 +157,31 @@ server <- function(input, output) {
   })
   
   output$interval <- renderText({
-    paste("90% confidence interval for change in Republican seats:", 
-          vals$lower, " to ", vals$upper)
+    paste("90% confidence interval for number of Democratic seats:", 
+          194 - vals$upper, " to ", 194 - vals$lower)
+  })
+  
+  output$est <- renderText({
+    paste("Estimated number of Democratic seats:", 
+          194 - round(vals$mean))
   })
   
   output$dem_win_pct <- renderText({
     paste("Probability of Democrats taking house:", 
-          round(pt((-24-vals$mean)/vals$se,df=vals$df) * 100),
+          round(pt((-23.5-vals$mean)/vals$se,df=vals$df) * 100),
           "%")
   })
   
   output$plot <- renderPlot({
-    set.seed(474747)
-    chrseats <- rt(10000, vals$df) * vals$se + vals$mean
-    dseats <- round(194 - chrseats)
-    forecast_df <- data.frame(dseats)
+    lower.bound <- 150
+    upper.bound <- 285
+    dist_df <- data.frame(dseats=c(lower.bound:upper.bound)) %>%
+      mutate(prob = 10000 * (pt((194-dseats+0.5-vals$mean)/vals$se,df=vals$df) - 
+                    pt((194-dseats-0.5-vals$mean)/vals$se,df=vals$df)))
+    forecast_df <- data.frame(dseats=rep(dist_df$dseats, dist_df$prob))
     ggplot(aes(x=dseats, fill=(dseats > 217)), data=forecast_df) + 
-      geom_histogram(binwidth=1) + xlab("Democratic seats in house") + 
-      ylab("Simulations") + scale_x_continuous(limits=c(150,285)) + 
+      geom_histogram(binwidth=1, aes(y=..count../sum(..count..))) + xlab("Democratic seats in house") + 
+      ylab("Probability") + scale_x_continuous(limits=c(150,285)) + 
       scale_fill_discrete(name=element_blank(),
                           breaks=c("FALSE", "TRUE"),
                           labels=c("Republican House", "Democratic House"))
