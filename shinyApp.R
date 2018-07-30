333bc8a42031c2ae6f10385203bea6567ed7751b
hbendekgey <henry.bendekgey@pomona.edu>
2018-07-30 16:32:21 -0700

    Rearrange two pages and add help links

--- shinyApp.R
+++ shinyApp.R
@@ -14,7 +14,11 @@ desc_text <- "The Structure-X model predicts the house elections using President
 shift_pred_text <- "For the past 6 elections (as long as Rothenberg has been rating races) this model has predicted outcomes more favorable to the Presidential party every election, on average by about 6 seats. With so few data points we don't have statistical significance, (p=0.06) and it's dangerous to overfit the data, so you should only check this if you think there's a systematic reason this model might overestimate incumbent performance."
 info_text <- "For more information on these models, check out <a href=\"https://github.com/hbendekgey/house_forecast_2018\">my Github</a>"
 npdi_desc_text <- "The National Poll District Info model predicts the national house popular vote based on generic ballot polls and who currently controls the presidency. From these structural conditions we expect Democrats to win 53.2% of the votes for representatives across the country, an environment very friendly to Democrats. The model then predicts how each seat will behave based on how they behaved in 2016, shifted to reflect the change in national environtment. Finally, it runs thousands of simulations: first picking a value for the national vote, then picking an outcome for each district based on that. Play around with some of the parameters and see how it affects the model!"
-
+trump_info_text <- "To make predictions for seats where incumbents are runnning, we take a combination of its house vote and presidential vote in 2016. This value represents the percentage of the prediction that comes from the presidential margin. Predicting the 2010 elections from 2008 suggest the value should be 42%, while predicting the 2014 elections from 2012 suggest it should be 12%. If you expect Republicans to do best in districts Trump did well in, increase this value. If you expect Republicans to do best in districts where incumbents did well in 2016, decrease this."
+open_adv_info_dec <- "This model assumes seats with incumbents running for relection and those without will swing (on average) by the same amount from 2016 to 2018. However, the large number of Republicans retiring (over double the number of Democrats) indicate that compared to their national margin, Democrats might overperform in open seats (and therefore underperform in incumbent seats). A simple calculation using incumbency advantage implies this advantage should be 1 percentage point (making the corresponding disadvantage 0.2 pct, because there are more incumbent seats than open ones). Play around and see how it changes predictions."
+open_stdev_desc <- "We know that our predictions aren't perfect, but we want to know by how much. Based on past elections, we expect the real results in seats without incumbents to be centered around our prediction with a standard deviation of 6.1. However, we have very few data points, so if you very much trust (or don't trust) this model, change this value."
+inc_stdev_desc <- "We know that our predictions aren't perfect, but we want to know by how much. Based on past elections, we expect the real results in seats with incumbents running to be centered around our prediction with a standard deviation of 4.5. However, we have very few data points, so if you very much trust (or don't trust) this model, change this value."
+nat_stdev_desc <- "We know that our predictions aren't perfect, but we want to know by how much. Based on past elections, we expect the true percentage of the national house votes that goes to the Democrats to be centered around our prediction with a standard deviation of 1.8. This relies on regression from elections since 1950, so if you believe this election to be atypical and hard to predict from precedent, increase this value. If you think this election will follow historic trends, decrease it."
 header <- dashboardHeader(
   title = "2018 House",
   tags$li(class = "dropdown", circleButton("info", icon("info"), size="sm", status="info"))
@@ -22,13 +26,58 @@ header <- dashboardHeader(
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Structure-X", tabName = "sx"),
-    menuItem("National Polls and District Info", tabName = "bafumi")
+    menuItem("National Polls and District Info", tabName = "bafumi"),
+    menuItem("Structure-X", tabName = "sx")
   )
 )
 
 body <- dashboardBody(useShinyalert(), fluidRow(
   tabItems(
+    # Bafumi, Erikson Wlezien ----
+    tabItem(tabName="bafumi",
+            column(8, align="center",
+                   box(width = NULL, align="left", status="primary", npdi_desc_text),
+                   box(width = NULL, 
+                       tabsetPanel(
+                         tabPanel("Histogram of House Seats", plotOutput("npdi_plot")),
+                         tabPanel("District-Level Data Table", dataTableOutput("district_info")),
+                         tabPanel("Tossup Districts", dataTableOutput("tossups"))
+                       )
+                   ),
+                   box(width = NULL, 
+                       h4(textOutput("npdi_interval")),
+                       h4(textOutput("npdi_est")),
+                       h4(textOutput("npdi_dem_win_pct"))
+                   )
+            ),
+            column(4,
+                   box(width = NULL, status = "warning",
+                       tags$h4(tags$b("Fix up the model your way")),
+                       sliderInput(inputId = "trump",
+                                   label = "How much is the GOP the Party of Trump?",
+                                   value = 0.42, min = 0, max = 1, step=0.01),
+                       actionLink("trump_info", "What does this mean?"),
+                       sliderInput(inputId = "open_adv",
+                                   label = "Expected Democratic overperformance in open seats",
+                                   value = 0, min = -1, max = 3, step=0.1),
+                       actionLink("open_adv_info", "Why would I want this?"),
+                       sliderInput(inputId = "open_stdev",
+                                   label = "Open Seat Uncertainty",
+                                   value = 6.1, min = 2, max = 10, step=0.1),
+                       actionLink("open_stdev_info", "Tell me more"),
+                       sliderInput(inputId = "inc_stdev",
+                                   label = "Incumbent Seat Uncertainty",
+                                   value = 4.5, min = 2, max = 10, step=0.1),
+                       actionLink("inc_stdev_info", "Tell me more"),
+                       sliderInput(inputId = "nat_stdev",
+                                   label = "National Vote Uncertainty",
+                                   value = 1.8, min = 1, max = 3, step=0.1),
+                       actionLink("nat_stdev_info", "Please, tell me more"),
+                       tags$br(),
+                       actionButton("npdi_reset", "Reset")
+                   )
+            )
+    ),
     # Structure-X ----
     tabItem(tabName="sx",
     column(8, align="center",
@@ -69,52 +118,7 @@ body <- dashboardBody(useShinyalert(), fluidRow(
         actionButton("reset", "Reset")
       )
     )
-  ),
-  # Bafumi, Erikson Wlezien ----
-  tabItem(tabName="bafumi",
-          column(8, align="center",
-                 box(width = NULL, align="left", status="primary", npdi_desc_text),
-                 box(width = NULL, 
-                     tabsetPanel(
-                       tabPanel("Histogram of House Seats", plotOutput("npdi_plot")),
-                       tabPanel("District-Level Data Table", dataTableOutput("district_info")),
-                       tabPanel("Tossup Districts", dataTableOutput("tossups"))
-                     )
-                 ),
-                 box(width = NULL, 
-                     h4(textOutput("npdi_interval")),
-                     h4(textOutput("npdi_est")),
-                     h4(textOutput("npdi_dem_win_pct"))
-                 )
-          ),
-          column(4,
-             box(width = NULL, status = "warning",
-                 tags$h4(tags$b("Fix up the model your way")),
-                 sliderInput(inputId = "trump",
-                             label = "How much is the GOP the Party of Trump?",
-                             value = 0.42, min = 0, max = 1, step=0.01),
-                 actionLink("trump_info", "What does this mean?"),
-                 sliderInput(inputId = "open_adv",
-                             label = "Expected Democratic overperformance in open seats",
-                             value = 0, min = -1, max = 3, step=0.1),
-                 actionLink("open-adv-info", "Why would I want this?"),
-                 sliderInput(inputId = "open_stdev",
-                             label = "Open Seat Uncertainty",
-                             value = 6.1, min = 2, max = 10, step=0.1),
-                 actionLink("open-stdev-info", "Tell me more"),
-                 sliderInput(inputId = "inc_stdev",
-                             label = "Incumbent Seat Uncertainty",
-                             value = 4.5, min = 2, max = 10, step=0.1),
-                 actionLink("inc-stdev-info", "Tell me more"),
-                 sliderInput(inputId = "nat_stdev",
-                             label = "National Vote Uncertainty",
-                             value = 1.8, min = 1, max = 3, step=0.1),
-                 actionLink("nat-stdev-info", "Please, tell me more"),
-                 tags$br(),
-                 actionButton("npdi_reset", "Reset")
-             )
-          )
-        ))
+  ))
 ))
 
 ui <- dashboardPage(header, sidebar, body)
@@ -259,7 +263,7 @@ server <- function(input, output, session) {
                           labels=c("Republican House", "Democratic House"))
   })
   
-  # NPDI 
+  ###NPDI 
   params18 <- data.frame(adj_genpoll=3.177, president_party=-1)
   dem_share16 <- 49.441-50
   
@@ -270,7 +274,28 @@ server <- function(input, output, session) {
     updateSliderInput(session, "inc_stdev", value=4.5)
     updateSliderInput(session, "nat_stdev", value=1.8)
   })
+  observeEvent(input$trump_info, {
+    shinyalert("Percent of Prediction Coming from Trump 2016 Margin", 
+               trump_info_text, type = "info")
+  })
+  observeEvent(input$open_adv_info, {
+    shinyalert("Considering Republican Retirements", 
+               open_adv_info_dec, type = "info")
+  })
+  observeEvent(input$open_stdev_info, {
+    shinyalert("Open Seat Standard Deviation", 
+               open_stdev_desc, type = "info")
+  })
+  observeEvent(input$inc_stdev_info, {
+    shinyalert("Incumbent Seat Standard Deviation", 
+               inc_stdev_desc, type = "info")
+  })
+  observeEvent(input$nat_stdev_info, {
+    shinyalert("National Vote Standard Deviation", 
+               nat_stdev_desc, type = "info")
+  })
   
+  # NPDI regression simulation and data
   sim <- reactive({
     set.seed(4747)
     fit <-  lm(vote ~ 0 + adj_genpoll + president_party, data=model)
@@ -307,6 +332,7 @@ server <- function(input, output, session) {
     data.frame(district, dem_share, dem_win_pct)[order(district),]
   })
   
+  # NPDI output
   output$npdi_plot <- renderPlot({
     forecast_df <- data.frame(dseats=dseats())
     ggplot(aes(x=dseats, fill=(dseats > 217)), data=forecast_df) + 
@@ -331,10 +357,10 @@ server <- function(input, output, session) {
           median(dseats()))
   })
   output$district_info <- renderDataTable({npdi_data()}, options = list(dom = 'ftpr'))
-  
   output$tossups <- renderDataTable({
     filter(npdi_data(), dem_win_pct > 40, dem_win_pct < 60)
   }, options = list(dom = 'ftpr'))
+  
 }
 
 shinyApp(ui = ui, server = server)
